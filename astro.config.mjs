5e95ee0b897a0aa93b5e542847f06ec72658f1b4
Jewgeni Lewash <j.lewash@gmail.com>
2024-02-28 07:52:46 +0100

    chore(deps): add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
+import icon from 'astro-icon';
 const SITE_URL = process.env.SITE_URL || 'https://astro.deployn.de';
 
 // https://astro.build/config
 export default defineConfig({
 	site: SITE_URL,
 	integrations: [
+		icon(),
 		react(),
 		sitemap(),
 		tailwind({
